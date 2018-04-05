f4e9ab0289a4b3b6fa9e6b6cc55a15a606c4c17b
PanktiParikh <panktip85@gmail.com>
2018-04-05 10:21:35 -0400

    initial organized project

---
+++ js/GuessingGame.js
@@ -0,0 +1,104 @@
+
+//random number generator
+function generateWinningNumber(){
+   return Math.floor(Math.random()*100) +1;
+}
+
+
+//shuffle the winning number for every game
+function shuffle(arr){
+    var arrLength = arr.length;
+    var t;
+    var i;
+
+    // While there remain elements to shuffle…
+    while(arrLength) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * arrLength--);
+
+        // And swap it with the current element.
+        t = arr[arrLength];
+        arr[arrLength] = arr[i];
+        arr[i] = t;
+    }
+    return arr;
+}
+
+//Game constructor function
+function Game(){
+    //players current guess
+    this.playersGuess = null;
+
+    //players past guesses as an array
+    this.pastGuesses = [];
+
+    //method to retrive the winning number
+    this.winningNumber = generateWinningNumber();
+}
+
+
+//returns the abolute value of difference between players guess and winning number
+Game.prototype.difference = function(){
+    return Math.abs(this.playersGuess - this.winningNumber);
+}
+
+//checks to see if the players guess is less than the winning number
+Game.prototype.isLower = function(){
+    if(this.playersGuess - this.winningNumber < 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+//assigns the players guess to this.playersGuess
+Game.prototype.playersGuessSubmission = function(num){
+   if(num < 1 || num > 100 || typeof(num) !== "number") {
+       throw "That is an invalid guess."
+   } else {
+        this.playersGuess = num;
+   } 
+   return this.checkGuess();   
+}
+
+
+//checks the guesses and provides feedback
+Game.prototype.checkGuess = function(){
+    console.log(this.difference())
+    
+    if(this.playersGuess === this.winningNumber){
+        return 'You Win!';
+    } else if(this.pastGuesses.indexOf(this.playersGuess)> -1){
+        return 'You have already guessed that number.';
+    } else if(this.pastGuesses.length < 4){
+        this.pastGuesses.push(this.playersGuess);
+    } else if(this.pastGuesses.length === 4){
+        return 'You Lose.';
+    } 
+    
+    if(this.difference() < 10){
+        return 'You\'re burning up!';
+    } else if(this.difference() < 25){
+        return 'You\'re lukewarm.'
+    } else if(this.difference() < 50){
+        return 'You\'re a bit chilly.'
+    } else if(this.difference() < 100){
+        return 'You\'re ice cold!';
+    }
+}
+
+//creates an instance of the new game
+function newGame(){
+    return new Game();
+}
+
+
+//hint provides 3 numbers (one of them is the winning number) and return arr is shuffled
+Game.prototype.provideHint = function(){
+    var arr =[this.winningNumber];
+    for(var i =0; i< 2; i++){
+        arr.push(generateWinningNumber());
+    }
+    return shuffle(arr);
+}
